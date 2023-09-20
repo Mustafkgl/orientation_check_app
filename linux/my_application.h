f839b9b09996eda22b08c66a0a26e8de9df7b96b
Mustafa Akgul <mustafakgl@proton.me>
2023-09-20 16:20:01 +0300

    Implement threshold detection logic

--- linux/my_application.h
+++ linux/my_application.h
@@ -7,3 +7,8 @@ G_DECLARE_FINAL_TYPE(MyApplication, my_application, MY, APPLICATION,
                      GtkApplication)
 
 /**
+ * my_application_new:
+ *
+ * Creates a new Flutter-based application.
+ *
+ * Returns: a new #MyApplication.
