db65ee87fc961c0f21cfe260ffd0d2fbd18a99b0
Mustafa Akgul <mustafakgl@proton.me>
2024-01-22 19:09:20 +0300

    Optimize detection loop timing

--- windows/runner/resource.h
+++ windows/runner/resource.h
@@ -6,3 +6,7 @@
 
 // Next default values for new objects
 //
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_COMMAND_VALUE         40001
